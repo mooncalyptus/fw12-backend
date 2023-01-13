3f9d262e40feb678a21391df68b3da4032131102
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2023-01-13 13:12:36 +0700

    set listening message

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ require('dotenv').config({
 const { urlencoded } = require('express')
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(morgan('dev'))
 app.use(cors())
 
 app.use('/', require('./src/routes'))
@@ -16,7 +18,7 @@ app.use("/uploads", express.static("uploads/"));
 app.get('/', (req,res) => {
     return res.status(200).json({
         success: true,
-        message: "Backend is running well"
+        message: "Tes Integrasi react native"
     })
 })
 
