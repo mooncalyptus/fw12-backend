01212b39f8a296925c6d882a42d883201d887498
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2023-02-02 11:51:29 +0700

    adding schedule movie query

--- src/models/movies.models.js
+++ src/models/movies.models.js
@@ -2,7 +2,7 @@ const db = require('../helpers/db.helpers')
 
 exports.displayMovies = (filter, cb) => {
   // const sql = `SELECT * FROM movies WHERE title LIKE $3 ORDER BY "${filter.sortBy}" ${filter.sort} LIMIT $1 OFFSET $2;`
-  const sql = `SELECT m.id, m.picture, m.title, m."releaseDate", m.duration, m.director, string_agg(g.name, ', ') AS genre, string_agg(c.name, ', ') AS casts, m.synopsis FROM movies m LEFT JOIN "movieGenre" mg ON mg."movieId" = m.id JOIN "genre" g ON g.id = mg."genreId" LEFT JOIN "movieCasts" mc ON mc."movieId" = m.id JOIN "casts" c ON c.id = mc."castsId" WHERE m.title LIKE $3 GROUP BY m.id ORDER BY "${filter.sortBy}" ${filter.sort} LIMIT $1 OFFSET $2`;
+  const sql = `SELECT m.id, m.picture, m.title, m."releaseDate", m.duration, m.director, string_agg(g.name, ', ') AS genre, string_agg(c.name, ', ') AS casts, m.synopsis, ci.name, ci.address, ms.price, string_to_array(string_agg(DISTINCT mst.time::VARCHAR, ', '), ', ') as time FROM "movieScheduleTime" mst JOIN "movieSchedule" ms ON mst."movieScheduleId" = ms.id JOIN cinemas ci on ci.id = ms."cinemasId" JOIN movies m ON ms."movieId" = m.id JOIN "movieGenre" mg ON mg."movieId" = m.id JOIN "genre" g ON g.id = mg."genreId" JOIN "movieCasts" mc ON mc."movieId" = m.id JOIN "casts" c ON c.id = mc."castsId" WHERE m.title LIKE $3 GROUP BY m.id, ms.price, ci.id, ms.id ORDER BY "${filter.sortBy}" ${filter.sort} LIMIT $1 OFFSET $2`;
   const values = [filter.limit, filter.offset, `%${filter.search}%`]
   db.query(sql, values, cb)
 }
